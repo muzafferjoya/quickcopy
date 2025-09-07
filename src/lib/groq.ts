c3d5405687094c6936a4f3a9fc84984c5c7b1c02
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2025-09-07 14:30:24 +0530

    Update groq.ts

--- src/lib/groq.ts
+++ src/lib/groq.ts
@@ -1,6 +1,6 @@
-import { OpenAI } from "openai";
+import { Groq } from "@groq/groq-sdk";
 
-export const groq = new OpenAI({
+export const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY!,
   baseURL: "https://api.groq.com/openai/v1",
 });
